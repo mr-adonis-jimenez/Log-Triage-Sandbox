dffc88e22718137ec1adcbd71b583879384783ae
Adonis Jimenez <Adonis.Jimenez@student.ctuonline.edu>
2026-01-17 03:53:10 -0500

    Refactor gulpfile.js to use ES6 module syntax
    Convert gulpfile.js from CommonJS (require/exports) to ES6 module syntax (import/export) to fix compatibility with package.json "type": "module". This resolves the build failure: ReferenceError: require is not defined in ES module scope.

--- gulpfile.js
+++ gulpfile.js
@@ -1,1 +1,7 @@
-const { series } = require('gulp'); function defaultTask(cb) { cb(); } exports.default = defaultTask;
+import { series } from 'gulp';
+
+function defaultTask(cb) {
+  cb();
+}
+
+export default defaultTask;
